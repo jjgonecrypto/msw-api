e15390f3f4ebe351d42e896f5d4b37a72885c2b2
justin j. moses <justinjmoses@gmail.com>
2013-12-16 14:06:34 -0500

    Adding `all()` functionality to `Forecast`

--- test/Forecast.spec.js
+++ test/Forecast.spec.js
@@ -7,15 +7,28 @@ var mocks = require('./mocks.js');
 var Forecast = require('../lib/Forecast.js');
 
 describe('Forecast', function() {
-    var forecast;
+    var forecast, mockData;
     beforeEach(function () {
+        mockData = mocks['2544'];
         forecast = new Forecast(mocks['2544']);
     });
 
     describe('toString()', function () {
         it('must output data in correct format', function () {
             //count number of output lines
-            expect(forecast.toString().split('\n')).to.have.length(mocks['2544'].length + 1);
+            expect(forecast.toString().split('\n')).to.have.length(mockData.length + 1);
+        });
+    });
+
+    describe('all()', function () {
+        it('must return all forecast entries', function () {
+            expect(forecast.all()).to.have.length(mockData.length);
+            expect(forecast.all()).to.be.instanceof(Array);
+        });
+        it('must return a copy of the forecast entries', function () {
+            var all = forecast.all();
+            all.pop();
+            expect(forecast.all()).to.have.length(mockData.length);
         });
     });
 });
