fdb157b3c95f8ac8816b3ee2f93f00432960110d
justin j. moses <justinjmoses@gmail.com>
2013-12-19 13:00:57 -0500

    Updating example

--- example/example.js
+++ example/example.js
@@ -1,13 +1,20 @@
 var msw = require('../lib/main.js');
 
 msw.set({
-    apiKey: 'YOUR API KEY GOES HERE',
+    apiKey: 'bL2dzxEV9mA99zV03VR5ChueHGiaivr0',
     units: 'US' //optional. one of: 'US', 'EU', 'UK'
 });
 
 msw.forecast(358).then(function (forecast) {
-    console.log('Successfully retrived data for Manasquan, NJ');
+    console.log('Swell Report for Manasquan, NJ');
     console.log(forecast.toString());
+
+    console.log('All entries with at least one faded star');
+    console.log(forecast.where({ minFadedStars: 1 }).toString());
+
+    console.log('All entries with wind less than 10mph and period at least 6s');
+    console.log(forecast.where({ maxWindSpeed: 10, minPeriod: 6 }).toString());
+
 }, function (err) {
     console.log('ERR: encountered error getting MSW data: ' + err);
 });
