23103c6c9bad1a398e5d92526f574c4488644da8
justin j. moses <justinjmoses@gmail.com>
2013-12-19 12:55:26 -0500

    Renaming `forecast.all()` to `forecast.toArray()`, ensuring `forecast.where()` returns a `Forecast` instance

--- README.md
+++ README.md
@@ -7,9 +7,7 @@ Node wrapper for Magicseaweed (MSW) developer API.
 
 [MSW API Docs](http://magicseaweed.com/developer/forecast-api)
 
-Quickstart
-------    
-
+##Quickstart
 
 ```javascript
 //Add the API to your module
@@ -35,55 +33,89 @@ msw.forecast(169).then(function (forecast) {
 });
 ```
 
+##API
 
-
-
-API
----
-
-###Set Static Configuration
+###msw.set(): static configuration
 
 ```javascript
+//signature
 msw.set(Object parameters) : msw;
 ```
 
-####apiKey
-
-Your MSW API key is required for any transactions. 
-
 ```javascript
+//apiKey: your MSW API key is required for any transactions. 
 msw.set({ apiKey: 'YOUR API KEY GOES HERE' });
-```
 
-####units
-Optionally set the units you want (option of US, EU &amp; UK). Default of 'US'. Case insensitive.
-
-```javascript
+//units: optionally set the units you want (option of US, EU &amp; UK). Default of 'US'. Case insensitive.
 msw.set({ units: 'EU' });
+
+//Obviously you can do both in one call
+msw.set({ apiKey: 'API KEY', units: 'UK' });
 ```
 
+>Go to [MSW](http://magicseaweed.com/developer/sign-up) to get an API key.
 
-###Retrieve Forecast
+###msw.forecast(): get swell forecast
 
 ```javascript
+//signature
 msw.forecast(int spotId | Object options) : Promises/A+ for Forecast
 ```
 
-Accepts ither:
+```javascript
 
-* integer parameter for spotID (and uses pre-set `units` via `set` or default value of 'US'); or
-* hash parameter for options
-    * `spotId` required
-    * `units` optional: overrides whatever pre-defined units are for this call only
+//Option 1: using spotId integer (uses pre-set `units` via `set()` or default value of 'US')
+msw.forecast(123);
 
-Throws: 
+//Option 2: using options object 
+//  spotId required
+//  units optional: overrides whatever pre-defined units are for this call only
+msw.forecast({ spotId: 123, units: 'eu' });
+```
 
-* if no parameter is given
-* if `apiKey` has not already been `set()`
+Throws `Error` when: 
 
-###Output Swell Data 
+* no parameter is given
+* `apiKey` not `set()`
+
+Returns:
+
+* promise with `Forecast` data. (Specifically returns a [Q](https://github.com/kriskowal/q) promise). 
 
 ```javascript
+msw.forecast(358).then(function (forecast) {
+    console.log('Successfully retrieved data for Manasquan, NJ');
+}, function (err) {
+    console.log('ERR: encountered error getting MSW data: ' + err);
+});
+```
+
+>Note: Errors in retrieving data from Magicseaweed, invalid API KEY and invalid spot ID handled by a rejected promise. Handle these errors in second argument to `then()` as per the Promises/A+ spec. 
+
+##Forecast 
+
+This is the instance yielded by a fulfilled `msw.forecast()` promise. 
+
+###forecast.toArray(): Swell data as Array
+```javascript
+//signature
+forecast.toArray() : Array
+```
+
+Returns an `Array` of `ForecastEntry`.
+
+```javascript   
+msw.forecast(358).then(function (forecast) {
+    var all = forecast.toArray();
+}, ...);
+```
+
+>Note: the Array is a clone of the underlying `Forecast` data, however each individual `ForecastEntry` is a mutable instance, so be wary of modifying this data.
+
+###forecast.toString(): Swell data as String 
+
+```javascript
+//signature
 forecast.toString([Optional] Object params) : String
 ```
 
@@ -91,23 +123,21 @@ Takes an optional parameter hash and returns a string with one line for each for
 
 ```javascript   
 msw.forecast(358).then(function (forecast) {
-    console.log('Successfully retrived data for Manasquan, NJ');
-    console.log(forecast.toString()); 
-}, function (err) {
-    console.log('ERR: encountered error getting MSW data: ' + err);
-});
+    console.log(forecast.toString());
+}, ...);
 ```
 
 Supports UTC times via:
 
 ```javascript
-forecast.toString({ utc: true });
+msw.forecast(358).then(function (forecast) {
+    console.log(forecast.toString({ utc: true }));
+}, ...);
 ```
 
-Outputs swell data in the following format:
+Returns swell data in the following format:
 
 ```
-Successfully retrived data for Manasquan, NJ
 Dec 13 01:00  ★       3-4ft (4.5ft 9s ENE)  12mph E     1F
 Dec 13 04:00  ★       3-4ft (4.5ft 9s ENE)  12mph E     1F
 Dec 13 07:00  ★       3-4ft (4.5ft 9s ENE)  12mph ENE   1F
@@ -126,39 +156,39 @@ Dec 14 19:00  ★       2-4ft (5ft 8s ENE)    10mph E     11F
 Dec 14 22:00  ★       2-4ft (5ft 8s ENE)    12mph E     11F
 ```
 
-###Query Forecast Data
+###forecast.where(): Query forecast data
+
+```javascript
+//signature
+forecast.where(Object params) : Forecast
+```
 
 ####Supported Parameters
 
 Can use any combination of the following for:
 
 * __Breaking height__ (the height of the swell in units): `minBreakingHeight` &/or `maxBreakingHeight` 
-* __Solid stars__ 
-* __Faded stars__
-* __Swell period__
-* __Wind speed__
-* __Wind direction__ (the direction of the wind represented as degrees in the range 0° - 360°): `lowerWindDirection` AND `upperWindDirection`. Both parameters are required to query on wind direction.
-* __Datetime__ `minDateTime`, `maxDateTime` (relative to the zone's timezone)
+* __Solid stars__ (MSW assigned solid stars): `minSolidStars` &/or `maxSolidStars`
+* __Faded stars__ (MSW assigned faded stars): `minFadedStars` &/or `maxFadedStars`
+* __Swell period__: (the number of seconds of the primary swell period): `minPeriod` &/or `maxPeriod`
+* __Wind speed__: (the direction of wind in units): `minWindSpeed` &/or `maxWindSpeed`
+* __Wind direction__ (the direction of the wind represented as degrees in the range 0° to 360°): `lowerWindDirection` AND `upperWindDirection`. __Both parameters are required to query on wind direction__.
+* __Datetime__: (the datetime of the forecast relative to the zone's timezone) `minDateTime` &/or `maxDateTime`
 
-####Min & Max inclusivity
+>Refer to [MSW Guide](http://magicseaweed.com/developer/forecast-api) for Solid & Faded star definitions
 
-All queries are inclusive. E.g. `minBreakingHeight` of 3 matches all entries `>=` 3
+>All queries are inclusive. E.g. `minBreakingHeight` of 3 matches all entries `>=` 3
 
-####Examples 
+```javascript
+//get all forecast entries with minBreakingHeight at least 3 units
+forecast.where({ minBreakingHeight: 3 });
 
-```javascript   
-msw.forecast(169).then(function (forecast) {
-    //returns all forecasts at least 5 solid stars and at least 6 
-    //(feet in this case as the request was using 'us' units) high, and at least 16s in primary swell period  
-    forecast.where
-        ({
-            minSolidStars: 5,
-            minBreakingHeight: 6,
-            minPeriod: 16
-        });
+//get all forecast entries with breaking height between 3 and 8, a wind speed of no more than 10 units and a period of at least 10s
+forecast.where({ minBreakingHeight: 3, maxBreakingHeight: 8, maxWindSpeed: 10, minPeriod: 10 });
 
-}, function (err) {
-    console.log('ERR: encountered error getting MSW data: ' + err);
-});
+//get all forecast entries with at least 3 solid and 1 faded stars
+forecast.where({ minSolidStars: 3, minFadedStars: 1 });
+
+//get all forecast entries with 5 or more solid stars, at least 6 in min breaking height and a 16 or more second period
+forecast.where({ minSolidStars: 5, minBreakingHeight: 6, minPeriod: 16 });
 ```
-
--- test/Forecast.spec.js
+++ test/Forecast.spec.js
@@ -22,15 +22,15 @@ describe('Forecast', function() {
         });
     });
 
-    describe('all()', function () {
+    describe('toArray()', function () {
         it('must return all forecast entries', function () {
-            expect(forecast.all()).to.have.length(mockData.length);
-            expect(forecast.all()).to.be.instanceof(Array);
+            expect(forecast.toArray()).to.have.length(mockData.length);
+            expect(forecast.toArray()).to.be.instanceof(Array);
         });
         it('must return a copy of the forecast entries', function () {
-            var all = forecast.all();
+            var all = forecast.toArray();
             all.pop();
-            expect(forecast.all()).to.have.length(mockData.length);
+            expect(forecast.toArray()).to.have.length(mockData.length);
         });
     });
 
@@ -40,67 +40,76 @@ describe('Forecast', function() {
             expect(function () { forecast.where({ something: 123 }); }).to.throw(Error);
         });
 
+        it('must return Forecast instance', function () {
+            expect(forecast.where({ minBreakingHeight: 0 })).to.be.instanceof(Forecast);
+        });
+
+        it('must allow empty queries', function () {
+            expect(forecast.where()).to.be.instanceof(Forecast);
+            expect(forecast.where({})).to.be.instanceof(Forecast);
+        });
+
         it('must support inclusive querying by MIN & MAX swell height', function () {
             //return all forecasts at or above 6 in height
-            expect(forecast.where({ minBreakingHeight: 6 })).to.have.length(28);
+            expect(forecast.where({ minBreakingHeight: 6 }).size()).to.equal(28);
 
             //return all forecasts at or below 10 in height
-            expect(forecast.where({ maxBreakingHeight: 10 })).to.have.length(26);
+            expect(forecast.where({ maxBreakingHeight: 10 }).size()).to.equal(26);
 
             //return all forecasts between 6 and 10 inclusive
-            expect(forecast.where({ minBreakingHeight: 6, maxBreakingHeight: 10 })).to.have.length(14);
+            expect(forecast.where({ minBreakingHeight: 6, maxBreakingHeight: 10 }).size()).to.equal(14);
 
             //return all forecasts between 7 and 7
-            expect(forecast.where({ minBreakingHeight: 7, maxBreakingHeight: 7 })).to.have.length(0);
+            expect(forecast.where({ minBreakingHeight: 7, maxBreakingHeight: 7 }).size()).to.equal(0);
         });
 
         it('must support inclusive querying by MIN & MAX solid stars', function () {
             //return all forecasts with 4 or more solid stars
-            expect(forecast.where({ minSolidStars: 4 })).to.have.length(33);
+            expect(forecast.where({ minSolidStars: 4 }).size()).to.equal(33);
             
             //return all forecasts with 3 or less solid stars
-            expect(forecast.where({ maxSolidStars: 3 })).to.have.length(7);
+            expect(forecast.where({ maxSolidStars: 3 }).size()).to.equal(7);
 
             //return all forecasts between 0 and 3 stars
-            expect(forecast.where({ minSolidStars: 0, maxSolidStars: 3 })).to.have.length(7);
+            expect(forecast.where({ minSolidStars: 0, maxSolidStars: 3 }).size()).to.equal(7);
 
             //return all 5 star forecasts
-            expect(forecast.where({ minSolidStars: 5, maxSolidStars: 5 })).to.have.length(24);
+            expect(forecast.where({ minSolidStars: 5, maxSolidStars: 5 }).size()).to.equal(24);
         });
 
 
         it('must suport inclusive querying by MIN & MAX faded stars', function () {
             //return all forecasts with 1 or more faded stars
-            expect(forecast.where({ minFadedStars: 1 })).to.have.length(16);
+            expect(forecast.where({ minFadedStars: 1 }).size()).to.equal(16);
             
             //return all forecasts with 3 or less faded stars
-            expect(forecast.where({ maxFadedStars: 3 })).to.have.length(37);
+            expect(forecast.where({ maxFadedStars: 3 }).size()).to.equal(37);
 
             //return all forecasts between 1 and 3 faded stars
-            expect(forecast.where({ minFadedStars: 1, maxFadedStars: 3 })).to.have.length(13);
+            expect(forecast.where({ minFadedStars: 1, maxFadedStars: 3 }).size()).to.equal(13);
         });
         
 
         it('must support inclusive querying by MIN & MAX period', function () {
             //return all forecasts with swell period 16s or more 
-            expect(forecast.where({ minPeriod: 16 })).to.have.length(16);
+            expect(forecast.where({ minPeriod: 16 }).size()).to.equal(16);
             
             //return all forecasts with swell period 16s or less
-            expect(forecast.where({ maxPeriod: 16 })).to.have.length(33);
+            expect(forecast.where({ maxPeriod: 16 }).size()).to.equal(33);
 
             //return all forecasts with swell period between 15-20s inclusively
-            expect(forecast.where({ minPeriod: 15, maxPeriod: 20 })).to.have.length(29);
+            expect(forecast.where({ minPeriod: 15, maxPeriod: 20 }).size()).to.equal(29);
         });
 
         it('must support inclusive querying by MIN & MAX wind speed', function () {
             //return all forecasts with wind speed greater than 10 
-            expect(forecast.where({ minWindSpeed: 10 })).to.have.length(30);
+            expect(forecast.where({ minWindSpeed: 10 }).size()).to.equal(30);
             
             //return all forecasts with wind speed less than 10
-            expect(forecast.where({ maxWindSpeed: 10 })).to.have.length(12);
+            expect(forecast.where({ maxWindSpeed: 10 }).size()).to.equal(12);
 
             //return all forecasts with wind speed between 0-5
-            expect(forecast.where({ minWindSpeed: 0, maxWindSpeed: 5 })).to.have.length(5);
+            expect(forecast.where({ minWindSpeed: 0, maxWindSpeed: 5 }).size()).to.equal(5);
         });
 
         it('must support inclusive querying by LOWER & UPPER wind direction', function () {
@@ -111,10 +120,10 @@ describe('Forecast', function() {
             expect(function () { forecast.where({ upperWindDirection: 10 }); }).to.throw(Error);
 
             //return all forecasts with wind direction between 355° and 5°
-            expect(forecast.where({ lowerWindDirection: 12, upperWindDirection: 70 })).to.have.length(18);
+            expect(forecast.where({ lowerWindDirection: 12, upperWindDirection: 70 }).size()).to.equal(18);
 
             //return all forecasts with wind direction between 355° and 5°
-            expect(forecast.where({ lowerWindDirection: 355, upperWindDirection: 10 })).to.have.length(6);
+            expect(forecast.where({ lowerWindDirection: 355, upperWindDirection: 10 }).size()).to.equal(6);
         });
         
         it('must support mixed querying for mulitple selection criteria', function () {
@@ -123,36 +132,36 @@ describe('Forecast', function() {
                     lowerWindDirection: 355,
                     upperWindDirection: 10,
                     maxWindSpeed: 10
-                })).to.have.length(1);
+                }).size()).to.equal(1);
 
             expect(forecast.where
                 ({
                     minSolidStars: 5,
                     minBreakingHeight: 6,
                     minPeriod: 16
-                })).to.have.length(15);
+                }).size()).to.equal(15);
         });
 
         it('must support inclusive querying by MIN & MAX local datetime', function () {
             //return all forecasts on or after a given date
-            expect(forecast.where({ minDateTime: moment.utc([2013, 11, 20]).toDate() })).to.have.length(24);
+            expect(forecast.where({ minDateTime: moment.utc([2013, 11, 20]).toDate() }).size()).to.equal(24);
 
             //return all forecasts on or before a given date
-            expect(forecast.where({ maxDateTime: moment.utc([2013, 11, 18, 23, 59]) })).to.have.length(8);
+            expect(forecast.where({ maxDateTime: moment.utc([2013, 11, 18, 23, 59]) }).size()).to.equal(8);
            
             //respects the time as well as date
-            expect(forecast.where({ maxDateTime: moment.utc([2013, 11, 18, 20, 59]) })).to.have.length(7);
+            expect(forecast.where({ maxDateTime: moment.utc([2013, 11, 18, 20, 59]) }).size()).to.equal(7);
             
             //return all forecasts between dates
             expect(forecast.where
                 ({
                     minDateTime: moment.utc([2013, 11, 22, 9]),
                     maxDateTime: moment.utc([2013, 11, 22, 16])
-                })).to.have.length(3);
+                }).size()).to.equal(3);
             
         });
         
-        //0.0.7 release
+        //0.0.8 release
         it('must support inclusive selection by MIN occurence of findings in a sequence');
         //return 3+ successive forecasts where all meet the given criteria
         //eg. forecast.where({ minSolidStars: 4, minSequence: 3 })
